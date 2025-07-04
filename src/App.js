4aec0a89ad49fc033e1bed958478efc5f4472341
Devannad <kannandevan007@gmail.com>
2025-07-04 21:31:12 +0530

    removed unwanted files and test dex api call

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useEffect } from 'react';
+import axios from 'axios';
 function App() {
+  useEffect(() => {
+    axios.get('https://api.dexscreener.com/token-profiles/latest/v1')
+      .then(response => {
+        console.log(response.data);
+      })
+      .catch(error => {
+        console.error('There was an error fetching the posts!', error);
+      });
+    console.log('App component mounted');
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
